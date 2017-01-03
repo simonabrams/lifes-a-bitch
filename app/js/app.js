cfbf27bf8629db4e5618db94c3e60de69fb8f0e2
Simon Abrams <sabrams@droga5.com>
2017-01-03 16:23:53 -0500

    Add refresh button. Increase number of song results

--- app/js/app.js
+++ app/js/app.js
@@ -1,43 +1,48 @@
 (function(){
+    //  set client ID token
+    var clientID = "EoG8HgZ6WIseNFKbw1qUxXo5w6lhrV1-f76IvGt4bfTfE2hlxSU2OHhBDA9kYADd";
     // set client access token
     var clientAccessToken = "Z-NIDSz5s1ET8GhOVI8I7qhWpkP5cJTPyxB1lHXPzYbtpJgXHTUvHRlJ5t7Kjg55";
     // search for songs with the lyric "life's a bitch"
-    var search_term = "life%27s+a+bitch";
-    var url = "https://api.genius.com/search?q=" + search_term + "&access_token=" + clientAccessToken;
+    var search_term = "lifes+a+bitch";
+    var url = "https://api.genius.com/search?q=" + search_term + "&per_page=50" + "&access_token=" + clientAccessToken;
 
     var data;
 
+    function getSongInfo(id) {
+
+    }
     function displayRandomResult(d) {
         // randomly select the song
         var rand = randRange(0, d.length);
         var song = d[rand];
         //display the song title, artist, album and lyric
         var s = {
-            title: song.result.title,
+            title: sentenceCase(song.result.title),
             artist: song.result.primary_artist.name,
             path: "https://genius.com" + song.result.path,
             artist_path: song.result.primary_artist.url,
-            thumbnail: song.result.song_art_image_thumbnail_url
+            thumbnail: song.result.song_art_image_thumbnail_url,
+            songInfo: "https://api.genius.com/song/" + song.result.id
         }
         console.log(song);
 
         // build the display 
         // TODO: Use handlebars for templating this
 
+        // set background image
         document.body.style.backgroundImage = "url(" + s.thumbnail + ")";
         document.body.style.backgroundRepeat = "no-repeat";
         document.body.style.backgroundSize = "cover";
 
-
-        // var thumbnailLink = document.getElementsByClassName('thumbnail-link')[0];
-        // thumbnailLink.href = s.path;
-
+        // set song title
         var titleText = document.getElementsByClassName('title-text')[0];
         titleText.innerHTML = s.title;
         
         var titleLink = document.getElementsByClassName('title-link')[0];
         titleLink.href = s.path;
 
+        // set artist
         var artist = document.getElementsByClassName('artist-text')[0];
         artist.innerHTML = s.artist;
         var artistLink = document.getElementsByClassName('artist-link')[0];
@@ -51,6 +56,15 @@
          return Math.round(min + (max - min) * Math.random());
     }
 
+    // set sentence case
+    function sentenceCase (str) {  
+      if ((str===null) || (str===''))  
+           return false;  
+      else  
+       str = str.toString();  
+      
+     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});  
+    }  
 
     // make a new XMLHttpRequest object
     var xhr;
@@ -60,11 +74,6 @@
         // windows IE
         xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    // open the connection to the server
-    xhr.open("GET", url, true);
-    // handle the data response
-    xhr.onreadystatechange = processResponse;
-    xhr.send();
 
     // handles the data returned by the server
     function processResponse(){
@@ -83,5 +92,21 @@
             console.log("Caught Exception: " + e.description);
         }
     }
+
+    // button handler
+    var songsButton = document.getElementsByClassName('songs-button')[0];
+    songsButton.addEventListener('mousedown', getSongs);
+
+    // gets songs from the API
+    function getSongs(){
+        // open the connection to the server
+        xhr.open("GET", url, true);
+        // xhr.setRequestHeader("Authorization", "Bearer " + clientAccessToken);
+        // handle the data response
+        xhr.onreadystatechange = processResponse;
+        xhr.send();
+    }
+
+    getSongs();
 
 })();
